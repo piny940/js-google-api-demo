3ca6327b0f46a38aec9eea841bccb3e9af8baebe
piny940 <83708535+piny940@users.noreply.github.com>
2024-04-03 16:41:46 +0900

    service accountでスプシの内容を読めるようにする

--- google/sheet.js
+++ google/sheet.js
@@ -1,15 +1,24 @@
+const { google } = require('googleapis')
+const { getAuth } = require('./auth')
+
 const checkRange = (range) => {
   if (!range.match(/[A-Z]+[0-9]+:[A-Z]+[0-9]+/)) {
     throw new Error("引数rangeの形式はA1:B2のようにしてください")
   }
 }
 const readSheet = async (range) => {
+  const auth = getAuth()
+  const client = google.sheets({ auth, version: "v4" })
+
   checkRange(range)
 
-  const ENDPOINT = `https://sheets.googleapis.com/v4/spreadsheets/${process.env.SPREADSHEET_ID}/values/${range}`
-  const response = await fetch(ENDPOINT + `?key=${process.env.GOOGLE_API_KEY}`)
-  const json = await response.json()
-  return json
+  const response = await client.spreadsheets.values.get({
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range,
+  })
+  const json = response.data
+  console.log(json)
+  return json.values
 }
 
 module.exports = {
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 require("dotenv").config()
+const auth = require("./google/auth.js")
 const sheet = require("./google/sheet.js")
 
 const main = async () => {
+  await auth.setup()
   console.log(await sheet.readSheet("C5:D7"))
 }
 
