6ea21e03ea2a5aab7d9bf3aa62b123ceb67a46bb
piny940 <83708535+piny940@users.noreply.github.com>
2024-04-03 14:42:12 +0900

    rangeの形式が正しいかチェックするようにする

--- google/sheet.js
+++ google/sheet.js
@@ -1,4 +1,11 @@
+const checkRange = (range) => {
+  if (!range.match(/[A-Z]+[0-9]+:[A-Z]+[0-9]+/)) {
+    throw new Error("引数rangeの形式はA1:B2のようにしてください")
+  }
+}
 const readSheet = async (range) => {
+  checkRange(range)
+
   const ENDPOINT = `https://sheets.googleapis.com/v4/spreadsheets/${process.env.SPREADSHEET_ID}/values/${range}`
   const response = await fetch(ENDPOINT + `?key=${process.env.GOOGLE_API_KEY}`)
   const json = await response.json()
